67fb5184e8b609f586ab419a71f96850ec562dd8
yong <wangyong813111@163.com>
2017-12-06 17:03:20 +0800

    init: 新建angular4项目

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './menu/sidebar/sidebar.component';
+import { TopComponent } from './menu/top/top.component';
+import { FooterComponent } from './menu/footer/footer.component';
+import { ContentComponent } from './menu/content/content.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    SidebarComponent,
+    TopComponent,
+    FooterComponent,
+    ContentComponent,
+    DashboardComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
